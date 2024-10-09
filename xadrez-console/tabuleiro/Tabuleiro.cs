84bb621dbba5626d2a3c99c7696471ae00d53ee4
Rafaela Ranieri Lassen <lassenrafaela@gmail.com>
2024-10-09 11:08:45 -0300

    começando a complementar o tabuleiro

---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xadrez_console.tabuleiro;
+
+namespace tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int linhas {  get; set; }   
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+        
+        //construtor:
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca [linhas, colunas];
+
+        }
+
+
+
+
+
+    }
+}
